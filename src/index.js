8f65a34b991f8702db65775782c2a02371ac3544
Nazar2015A <nazar.komar456@gmail.com>
2023-10-15 12:12:17 +0300

    Optimization images

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { initialState } from "./context/initialState";
 import reducer from "./context/reducer";
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
